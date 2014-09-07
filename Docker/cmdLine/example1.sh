40399b1992e7de5ac69cf6150c7e8049803999ce
Johan MOREAU <Johan.Moreau@gmail.com>
2014-09-07 11:15:02 +0200

    Add example for docker

---
+++ Docker/cmdLine/example1.sh
@@ -0,0 +1,12 @@
+sudo /usr/bin/docker -d & # run the daemon
+sudo docker search ubuntu # give ubuntu images from public index (official/trusted)
+sudo docker pull stackbrew/ubuntu # pull latest stackbrew/ubuntu images
+sudo docker history stackbrew/ubuntu # view history for this images
+sudo docker images # show local images
+sudo docker run -i -t stackbrew/ubuntu /bin/bash # run this images / create container
+sudo docker run -t -i -link redis:db -name webapp ubuntu bash # link 2 containers
+sudo docker ps # show active containers (-a to show all containers)
+sudo docker logs myUbuntu
+sudo docker attach myUbuntu # retake the hand on the container
+sudo docker run -d -p 8888:80 ubuntu # export 8888 on master
+
