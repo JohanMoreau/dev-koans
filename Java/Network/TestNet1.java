9ee04313c74059c357d584c8b839d8a1cac7bab1
Johan Moreau <johan.moreau@gmail.com>
2017-01-03 17:00:59 +0100

    Minor fix in a loop in network example for Java

--- Java/Network/TestNet1.java
+++ Java/Network/TestNet1.java
@@ -8,7 +8,7 @@ public class TestNet1 {
             System.out.println("Adresse Sun = "+adrServeur.getHostAddress());
             InetAddress[] adrServeurs = InetAddress.getAllByName("www.google.com");
             System.out.println("Adresses Google: ");
-            for (int i = 0; i > adrServeurs.length; i++) {
+            for (int i = 0; i < adrServeurs.length; i++) {
                 System.out.println(""+adrServeurs[i].getHostAddress());
             }
         } catch (Exception e) {
